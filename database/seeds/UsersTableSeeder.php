9d5c50dc149951e0b53e9eb0fbd077ab6c298835
katiepalumbo <palumbo.katie@gmail.com>
2022-06-03 12:46:59 +0200

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use phpDocumentor\Reflection\DocBlock\Tags\Reference\Url;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        //utilizziamo questo seeder per assicurarci che l'utente Luke con password "skywalker" esiste già nel database
+
+        $users = [
+            [
+                "username" => "Luke",
+                "email" => "luke@skywalker.com",
+                "name" => "Luke",
+                "lastname" => "Skywalker",
+            ],
+            
+        ];
+
+        foreach($users as $user) {
+
+            $newUser = new User();
+
+            $newUser->username = $user["username"];
+            $newUser->name = $user["name"];
+            $newUser->lastname = $user["lastname"];
+            $newUser->email = $user["email"];
+            $newUser->password = Hash::make('skywalker');
+            $newUser->slug = Str::slug($newUser->username);
+            $counter = 1;
+            while (User::where('slug', $newUser->slug)->first()) {
+                $newUser->slug = Str::slug($newUser->username) . '-' . $counter;
+                $counter++;
+            }
+
+            $newUser->save();
+
+        }
+    }
+}
+
---
+++ resources/js/views/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>WORK IN PROGRESS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+
+}
+</script>
+
+<style>
+
+</style>
